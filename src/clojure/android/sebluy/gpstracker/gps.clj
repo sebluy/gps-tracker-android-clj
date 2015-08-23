9e846ea8b616ec49e96ee5a0f6c1b34627595166
Sebastian Luy <sebastian.a.luy@gmail.com>
2015-08-23 16:51:31 -0400

    Fix tracker ui

--- src/clojure/android/sebluy/gpstracker/gps.clj
+++ src/clojure/android/sebluy/gpstracker/gps.clj
@@ -1,16 +1,30 @@
 (ns android.sebluy.gpstracker.gps
   (:require [neko.activity :as activity]
-            [neko.threading :as threading]))
+            [neko.ui.mapping :as mapping]
+            [neko.threading :as threading])
+  (:import (android.widget TableLayout
+                           TableRow)
+           (android.view WindowManager$LayoutParams)))
+
+(mapping/defelement
+  :table-layout
+  :classname TableLayout
+  :inherits :view-group)
+
+(mapping/defelement
+  :table-row
+  :classname TableRow
+  :inherits :view)
 
 (defn table-row
   ([title] (table-row title "Pending"))
   ([title value]
-   [:table-row
+   [:table-row {}
     [:text-view {:text title}]
     [:text-view {:text value}]]))
 
 (defn tracking-table []
-  (into [:table-layout]
+  (into [:table-layout {}]
         (map table-row
              ["Status"
               "Current Speed"
@@ -18,9 +32,23 @@
               "Total Distance"
               "Duration"])))
 
+(defn ui [activity]
+  [:linear-layout {}
+   (tracking-table)
+   [:button {:text "Pause"}]
+   [:button {:text "Finish"
+             :on-click (fn [_] (.finish activity))}]])
+
 (defn render-ui [activity]
   (threading/on-ui
-    (activity/set-content-view! activity (tracking-table))))
+    (activity/set-content-view! activity (ui activity))))
+
+(defn keep-screen-on [activity boolean]
+  (threading/on-ui
+    (let [window (.getWindow activity)]
+      (if boolean
+        (.addFlags window WindowManager$LayoutParams/FLAG_KEEP_SCREEN_ON)
+        (.clearFlags window WindowManager$LayoutParams/FLAG_KEEP_SCREEN_ON)))))
 
 (activity/defactivity
   android.sebluy.gpstracker.TrackingActivity
@@ -28,6 +56,13 @@
   (onCreate
     [this bundle]
     (.superOnCreate this bundle)
-    (render-ui this)))
-
+    (render-ui this))
+  (onStart
+    [this]
+    (.superOnStart this)
+    (keep-screen-on this true))
+  (onStop
+    [this]
+    (.superOnStop this)
+    (keep-screen-on this false)))
 
