aa86036043b9ae3deeb6f9ed3a9655716e80314d
Sebastian Luy <sebastian.a.luy@gmail.com>
2015-09-27 22:50:15 -0400

    Start to add some order to this hairball

--- src/clojure/android/sebluy/gpstracker/remote.clj
+++ src/clojure/android/sebluy/gpstracker/remote.clj
@@ -19,7 +19,32 @@
                 (select-keys point #{:latitude :longitude :speed :accuracy})))
          (into []))]])
 
-(defn post [^String body]
+(post (pr-str [[:get-waypoint-path-ids]]))
+
+(defn write-connection [connection ^String body]
+  (try
+    (let [output-stream (BufferedOutputStream. (.getOutputStream connection))]
+      (try
+        (doto output-stream
+          (.write (.getBytes body))
+          (.flush))
+        (finally
+          (.close output-stream))))))
+
+(defn read-response [reader]
+  (let [line-sequence (repeatedly #(.readLine reader))]
+    (reduce str "" (take-while some? line-sequence))))
+
+(defn read-connection [connection]
+  (let [input-reader (BufferedReader. (InputStreamReader. (.getInputStream connection)))]
+    (try
+      (let [response-code (.getResponseCode connection)
+            response (read-response input-reader)]
+        [response-code response])
+      (finally
+        (.close input-reader)))))
+
+(defn post [body]
   (let [url (URL. "https://fierce-dawn-3931.herokuapp.com/api")
         ^HttpURLConnection connection (.openConnection url)]
     (doto connection
@@ -27,23 +52,8 @@
       (.setDoInput true)
       (.setRequestProperty "Content-Type" "application/edn"))
     (try
-      (let [output-stream (BufferedOutputStream. (.getOutputStream connection))]
-        (try
-          (do (doto output-stream
-                (.write (.getBytes body))
-                (.flush)))
-          (finally
-            (.close output-stream))))
-      (let [input-reader (BufferedReader. (InputStreamReader. (.getInputStream connection)))]
-        (try
-          (let [response-code (.getResponseCode connection)
-                response (loop [response ""]
-                           (if-let [response-chunk (.readLine input-reader)]
-                             (recur (str response response-chunk))
-                             response))]
-            [response-code response])
-          (finally
-            (.close input-reader))))
+      (do (write-connection connection body)
+          (read-connection connection))
       (catch Exception ex ex)
       (finally
         (.disconnect connection)))))
@@ -64,7 +74,7 @@
    {:orientation :vertical}
    [:text-view {:text msg}]
    [:button {:text     "Retry"
-             :on-click (fn [_] (upload-path activity (@state/state :path)))}]])
+             :on-click (fn [_] (state/handle upload-path activity))}]])
 
 (defn render-ui [activity status]
   (threading/on-ui
@@ -81,17 +91,26 @@
         network-info (.getActiveNetworkInfo ^ConnectivityManager connectivity)]
     (and network-info (.isConnected network-info))))
 
-(defn upload-path [activity path]
-  (if (network-available? activity)
-    (do
-      (render-ui activity :loading)
-      (future
-        (let [result (-> path path->action pr-str post)]
-          (threading/on-ui
-            (if (= result 200)
-              (render-ui activity :success)
-              (render-ui activity :failure))))))
-    (render-ui activity :disconnected)))
+(defn update-ui [activity state]
+  (render-ui activity (get-in state [:remote :status])))
+
+(defn handle-response [activity [response-code _] state]
+  (let [new-status (if (= response-code 200) :success :failure)
+        new-state (assoc-in state [:remote :status] new-status)]
+    (update-ui activity new-state)
+    new-state))
+
+(defn upload-path [activity state]
+  (let [network? (network-available? activity)
+        new-status (if network? :pending :disconnected)
+        new-state (assoc-in state [:remote :status] new-status)]
+    (when network?
+      (let [path (get-in state [:remote :path])]
+        (future
+          (let [response-attrs (-> path path->action pr-str post)]
+            (state/handle handle-response activity response-attrs)))))
+    (update-ui activity new-state)
+    new-state))
 
 #_(defn get-waypoints [activity]
     (if (network-available? activity)
@@ -111,7 +130,7 @@
   (onCreate
     [this bundle]
     (.superOnCreate this bundle)
-    (upload-path this (@state/state :upload)))
+    (state/handle upload-path this))
   (onStart
     [this]
     (.superOnStart this)
--- src/clojure/android/sebluy/gpstracker/state.clj
+++ src/clojure/android/sebluy/gpstracker/state.clj
@@ -2,4 +2,11 @@
 
 (def state (atom {}))
 
-@state
+(defn handle [handler-fn & args]
+  (swap! state (apply partial handler-fn args)))
+
+(handle set-sum 2 3)
+
+(defn set-sum [x y state]
+  (assoc state :sum (+ x y)))
+
